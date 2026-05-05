ba9655e3b395bb9b6e2cb0dceca2f1566d6540bc
nureencenturion <centurionureen@gmail.com>
2026-05-05 09:12:39 -0400

    Fiz o insert no dao.

--- src/dao/DaoMpvUsuarios.java
+++ src/dao/DaoMpvUsuarios.java
@@ -4,6 +4,17 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import testes.JdbcCrud;
+import bean.MpvUsuarios;
+import java.sql.Date;
+
 /**
  *
  * @author u71744222177
@@ -12,27 +23,82 @@ public class DaoMpvUsuarios extends DaoAbstract{
 
     @Override
     public void insert(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       MpvUsuarios mpvUsuarios = (MpvUsuarios) object;
+       try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url, user, password;
+            url="jdbc:mysql://10.7.0.51:33062/db_marcos_vilhanueva";
+            user="marcos_vilhanueva";
+            password="marcos_vilhanueva";
+            
+            Connection cnt;
+            cnt = DriverManager.getConnection(url, user, password); 
+            
+
+            String sql = "insert into mpv_usuarios values(?,?,?,?,?,?,?,?)";
+            PreparedStatement pst = cnt.prepareStatement(sql);    
+            pst.setInt(1, mpvUsuarios.getMpvIdUsuarios());
+            pst.setString(2, mpvUsuarios.getMpvNome());
+            pst.setString(3, mpvUsuarios.getMpvApelido());
+            pst.setString(4, mpvUsuarios.getMpvCpf());
+            pst.setDate(5, null);// mpvUsuarios.getMpvDataNascimento());
+            pst.setInt(6, mpvUsuarios.getMpvNivel());
+            pst.setString(7, mpvUsuarios.getMpvSenha());
+            pst.setString(8, mpvUsuarios.getMpvAtivo());
+            pst.executeUpdate();
+            
+                                        
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(JdbcCrud.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(JdbcCrud.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void update(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       MpvUsuarios mpvUsuarios = (MpvUsuarios) object; 
     }
 
     @Override
     public void delete(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       MpvUsuarios mpvUsuarios = (MpvUsuarios) object; 
     }
 
     @Override
     public Object list(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return null;
     }
 
     @Override
     public Object listAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url, user, password;
+            url="jdbc:mysql://10.7.0.51:33062/db_marcos_vilhanueva";
+            user="marcos_vilhanueva";
+            password="marcos_vilhanueva";
+            
+            Connection cnt;
+            cnt = DriverManager.getConnection(url, user, password); 
+            
+
+            String sql = "select * from mpv_usuarios ";
+            PreparedStatement pst = cnt.prepareStatement(sql);    
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()== true){
+                System.out.println("id:" + rs.getInt("mpv_idusuarios"));
+                System.out.println("nome:" + rs.getString("mpv_nome"));
+                
+            }
+            
+                                        
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(JdbcCrud.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(JdbcCrud.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return null;
     }
     
 }
--- src/testes/JbdcSelect.java
+++ src/testes/JbdcSelect.java
@@ -4,19 +4,19 @@
  */
 package testes;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author u71744222177
  */
-public class JdbcCrud {
+public class JbdcSelect {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -24,26 +24,19 @@ public class JdbcCrud {
             url="jdbc:mysql://10.7.0.51:33062/db_marcos_vilhanueva";
             user="marcos_vilhanueva";
             password="marcos_vilhanueva";
-//            url="jdbc://mysql/localhost/db_nureen_centurion";
-//            user="nureen_centurion";
-//            password="nureen_centurion";
+            
             Connection cnt;
             cnt = DriverManager.getConnection(url, user, password); 
-            //110   
-            //Statement stm = cnt.createStatement();
-            //stm.executeUpdate("insert into mpv_usuarios (mpv_idusuarios, mpv_nome, mpv_apelido, mpv_cpf ) values(110, 'nureen', 'nuri', '781.788.645-56')");
             
-//            String sql = "update mpv_usuarios set mpv_nome=?,"
-//                    + "mpv_apelido=?, mpv_cpf=?"
-//                    + "where mpv_idusuarios=?";
 
-            String sql = "delete from mpv_usuarios where mpv_idusuarios =?";
-            PreparedStatement pst = cnt.prepareStatement(sql);
-            pst.setInt(1,210);        
-//            pst.setString(1, "Nureeeen");
-//            pst.setString(2, "nrc");
-//            pst.setString(3, "4759387098");
-            pst.executeUpdate();
+            String sql = "select * from mpv_usuarios ";
+            PreparedStatement pst = cnt.prepareStatement(sql);    
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()== true){
+                System.out.println("id:" + rs.getInt("mpv_idusuarios"));
+                System.out.println("nome:" + rs.getString("mpv_nome"));
+                
+            }
             
                                         
         } catch (ClassNotFoundException ex) {
